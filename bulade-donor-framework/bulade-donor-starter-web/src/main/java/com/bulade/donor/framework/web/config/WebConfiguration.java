7da6aebdabec775c182a9b12801a499ea3488ea7
nannan <18830911262@163.com>
2024-02-19 16:17:11 +0800

    WebConfiguration

--- bulade-donor-framework/bulade-donor-starter-web/src/main/java/com/bulade/donor/framework/web/config/WebConfiguration.java
+++ bulade-donor-framework/bulade-donor-starter-web/src/main/java/com/bulade/donor/framework/web/config/WebConfiguration.java
@@ -5,19 +5,29 @@ import com.bulade.donor.framework.web.apilog.service.ApiErrorLogFrameworkService
 import com.bulade.donor.framework.web.core.BuladeFastJsonHttpMessageConverter;
 import com.bulade.donor.framework.web.core.filter.CacheRequestBodyFilter;
 import com.bulade.donor.framework.web.core.handler.GlobalExceptionHandler;
+import com.bulade.donor.framework.web.utils.WebFrameworkUtils;
+import jakarta.annotation.Resource;
 import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import com.alibaba.fastjson2.support.config.FastJsonConfig;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -25,15 +35,42 @@ import java.util.List;
 import static com.alibaba.fastjson2.JSONWriter.Feature.WriteNullNumberAsZero;
 import static com.alibaba.fastjson2.JSONWriter.Feature.WriteNullStringAsEmpty;
 
-@Configuration
-public class WebConfiguration {
+@AutoConfiguration
+@EnableConfigurationProperties(WebProperties.class)
+public class WebConfiguration implements WebMvcConfigurer {
 
     @Value("${spring.application.name}")
     private String applicationName;
 
+    @Resource
+    private WebProperties webProperties;
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurePathMatch(configurer, webProperties.getAdminApi());
+    }
+
+    /**
+     * 设置 API 前缀，仅仅匹配 controller 包下的
+     *
+     * @param configurer 配置
+     * @param api        API 配置
+     */
+    private void configurePathMatch(PathMatchConfigurer configurer, WebProperties.Api api) {
+        AntPathMatcher antPathMatcher = new AntPathMatcher(".");
+        configurer.addPathPrefix(api.getPrefix(), clazz -> clazz.isAnnotationPresent(RestController.class)
+            && antPathMatcher.match(api.getController(), clazz.getPackage().getName())); // 仅仅匹配 controller 包
+    }
+
     @Bean
     public GlobalExceptionHandler globalExceptionHandler(ApiErrorLogFrameworkService apiErrorLogFrameworkService) {
         return new GlobalExceptionHandler(applicationName, apiErrorLogFrameworkService);
+    }
+
+    @Bean
+    public WebFrameworkUtils webFrameworkUtils(WebProperties webProperties) {
+        // 由于 WebFrameworkUtils 需要使用到 webProperties 属性，所以注册为一个 Bean
+        return new WebFrameworkUtils(webProperties);
     }
 
     @Bean
@@ -80,6 +117,16 @@ public class WebConfiguration {
         registration.setFilter(filter);
         registration.setOrder(order);
         return registration;
+    }
+
+    /**
+     * 创建 RestTemplate 实例
+     *
+     * @param restTemplateBuilder {@link RestTemplateAutoConfiguration#restTemplateBuilder}
+     */
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 
 }
